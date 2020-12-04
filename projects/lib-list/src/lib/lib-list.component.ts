aee987b1c7c75e7c17344698d8c850f61b1287a0
AaronVN <72452145+AaronVN@users.noreply.github.com>
2020-12-04 09:39:49 +0100

    Añadidas capacidades a libreria List (no aplicacadas por error de caught In promise)

--- projects/lib-list/src/lib/lib-list.component.ts
+++ projects/lib-list/src/lib/lib-list.component.ts
@@ -8,8 +8,37 @@ import { LibListService } from './lib-list.service';
   styleUrls: ['./lib-list.component.css']
 })
 export class LibListComponent {
-  constructor() {
-    }
+  showingAvailable !: boolean; // 0 = available, 1 = pending, 2 = sold
+  showingPending !: boolean; // "!" para inicializar una variable no definida
+  showingSold !: boolean;
+
+  empty: null;
+
+  constructor(private lib: LibListService) {}
+  pets: any = [];
+  ngOnInit(): void {
+    this.showingAvailable = true;
+    this.showingPending = false;
+    this.showingSold = false;
+    this.pets = this.lib.getList('available');
+    console.log('Recibida array pets: ');
+    console.log(this.pets);
+  }
+
+  // Buttons
+  setListTypeD() {// Metodo para vaciar array
+                  this.pets = this.lib.getList('available');
+                  this.showingAvailable = true; this.showingPending = false; this.showingSold = false;
+                }
+  setListTypeP() {
+                  this.pets = this.lib.getList('pending');
+                  this.showingAvailable = false; this.showingPending = true; this.showingSold = false;
+  }
+  setListTypeV() {
+                  this.pets = this.lib.getList('sold');
+                  this.showingAvailable = true; this.showingPending = false; this.showingSold = false;
+   }
+
 
 
 
--- projects/lib-list/src/lib/lib-list.module.ts
+++ projects/lib-list/src/lib/lib-list.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { LibListComponent } from './lib-list.component';
 
 @NgModule({
   declarations: [LibListComponent],
-  imports: [
+  imports: [ CommonModule
   ],
   exports: [LibListComponent]
 })
